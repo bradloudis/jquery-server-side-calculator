c10a5e7415c6376ce388b28b6d3270c8367d5f58
Brad Loudis <Bradley.Loudis@gmail.com>
2020-10-16 16:23:16 -0500

    add in bodyParser. set up and test app.post('/calculation')

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static('public'));
+let calcInfo = [];
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// ROUTES BELOW HERE
+
+// TODO GET
+
+// TODO POST
+app.post('/calculation', (req, res) => {
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
+app.use(express.static('public'));
 app.listen(PORT, () => {
   console.log('Listening on PORT: ', PORT);
 });
