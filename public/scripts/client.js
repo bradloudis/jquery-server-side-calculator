a35e1a7dfe831b7d97a984e6bbb9d51e78290385
Brad Loudis <Bradley.Loudis@gmail.com>
2020-10-16 18:44:11 -0500

    added name attribute to all math operator btns. click handler for btns call operatorSelector(). name attribute added to calcObject

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,7 +1,29 @@
 $(document).ready(readyUp);
 
+let operator = '';
+
 function readyUp() {
-  console.log('JQ');
+  // console.log('JQ');
+  $('.js-equalBtn').on('click', clickEqualSubmit);
+  $('.js-addBtn').on('click', operatorSelector);
+  $('.js-minusBtn').on('click', operatorSelector);
+  $('.js-multiplyBtn').on('click', operatorSelector);
+  $('.js-divideBtn').on('click', operatorSelector);
+}
+
+function clickEqualSubmit() {
+  console.log('click me baby one more time!');
+  const calcObject = {
+    numOne: $('.js-numOne').val(),
+    operator: operator,
+    numTwo: $('.js-numTwo').val(),
+  };
+  postCalculations(calcObject);
+}
+
+function operatorSelector() {
+  operator = $(this).prop('name');
+  return operator;
 }
 
 // SERVER CALLS
@@ -13,7 +35,7 @@ function postCalculations(calcObject) {
     data: calcObject,
   })
     .then((response) => {
-      console.log('buttz');
+      console.log(response);
     })
     .catch((err) => {
       console.log(err);
